fe0c72e6a4c2f835e23252936591452668a0711c
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-16 17:54:59 -0400

    Entity Venda

---
+++ src/main/java/br/ufms/facom/techms/model/entity/Venda.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.ufms.facom.techms.model.entity;
+
+/**
+ *
+ * @author gustavo
+ */
+import java.util.Date;
+import java.util.List;
+
+public class Venda {
+    private int id;
+    private Date data;
+    private String formaPagamento;
+    private double valorTotal;
+    private Funcionario funcionario;
+    private Cliente cliente;
+    private List<ItemVenda> itens;
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(String formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<ItemVenda> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ItemVenda> itens) {
+        this.itens = itens;
+    }
+}
