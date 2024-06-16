82775ec5ac522ea181389642f19005248467e8aa
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-16 17:55:40 -0400

    Entity Cliente

---
+++ src/main/java/br/ufms/facom/techms/model/entity/Cliente.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.ufms.facom.techms.model.entity;
+
+/**
+ *
+ * @author gustavo
+ */
+
+public class Cliente {
+    private String email;
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+    // Getters e Setters
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
