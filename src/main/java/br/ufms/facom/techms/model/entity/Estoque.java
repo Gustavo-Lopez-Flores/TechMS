77772a00e9fb97ad13348da60fd296fcf7bef56e
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-16 17:58:26 -0400

    Entity Estoque

---
+++ src/main/java/br/ufms/facom/techms/model/entity/Estoque.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.ufms.facom.techms.model.entity;
+
+/**
+ *
+ * @author gustavo
+ */
+
+public class Estoque {
+    private int id;
+    private int quantidade;
+    private String localizacao;
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+}
