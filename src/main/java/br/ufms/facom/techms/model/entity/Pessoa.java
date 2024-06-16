aeade9c14e591857e29a5436212de5a5e787c087
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-16 17:53:06 -0400

    Entity Pessoa

---
+++ src/main/java/br/ufms/facom/techms/model/entity/Pessoa.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.ufms.facom.techms.model.entity;
+
+/**
+ *
+ * @author gustavo
+ */
+
+public class Pessoa {
+    private String nome;
+    private String cpf;
+    private String rg;
+    private String login;
+    private String senha;
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
