2e69ead2b922b95c9f74c832c8c391f19db7a611
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-16 18:02:00 -0400

    Entity Fornecedor

---
+++ src/main/java/br/ufms/facom/techms/model/entity/Fornecedor.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.ufms.facom.techms.model.entity;
+
+/**
+ *
+ * @author gustavo
+ */
+public class Fornecedor {
+    private int id;
+    private String nome;
+    private String contato;
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+}
