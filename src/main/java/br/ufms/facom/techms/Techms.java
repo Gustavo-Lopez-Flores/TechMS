7c79cc1d025937de7df5c7535548d4c118b3463c
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-16 20:34:11 -0400

    new class

--- src/main/java/br/ufms/facom/techms/Techms.java
+++ src/main/java/br/ufms/facom/techms/Techms.java
@@ -9,6 +9,8 @@ import br.ufms.facom.techms.model.dao.ProdutoDAO;
 import br.ufms.facom.techms.model.service.ProdutoService;
 import br.ufms.facom.techms.view.ProdutoView;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
@@ -16,9 +18,10 @@ import java.sql.Connection;
  */
 public class Techms {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // Inicialização do banco de dados e injeção de dependências
         Connection connection = null; // Obtenha uma conexão com o banco de dados
+        connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/techms", "postgres", "postgres");
         ProdutoDAO produtoDAO = new ProdutoDAO(connection);
         ProdutoService produtoService = new ProdutoService(produtoDAO);
         ProdutoController produtoController = new ProdutoController(produtoService);
