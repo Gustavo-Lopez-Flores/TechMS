715cb5f2d77341826171e4b4e424f0b6ca9fbf92
Gustavo Lopez Flores <gustavo.flores@dominio.ufms.br>
2024-06-18 18:27:07 -0400

    criação de funcionarioDAO

---
+++ src/main/java/br/ufms/facom/techms/model/dao/FuncionarioDAO.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.ufms.facom.techms.model.dao;
+
+/**
+ *
+ * @author gustavo.flores
+ */
+import br.ufms.facom.techms.model.entity.Funcionario;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FuncionarioDAO {
+    private Connection connection;
+
+    public FuncionarioDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public List<Funcionario> readAll() throws SQLException {
+        List<Funcionario> funcionarios = new ArrayList<>();
+        String sql = "SELECT id, nome FROM funcionario";
+        try (PreparedStatement stmt = connection.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+            while (rs.next()) {
+                Funcionario funcionario = new Funcionario();
+                funcionario.setId(rs.getInt("id"));
+                funcionario.setNome(rs.getString("nome"));
+                funcionarios.add(funcionario);
+            }
+        }
+        return funcionarios;
+    }
+}
