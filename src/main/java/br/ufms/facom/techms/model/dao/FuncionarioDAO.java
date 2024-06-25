5537eba1ca5c0289988672a442a5cbac27c26607
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-24 20:39:07 -0400

    correção readAll

--- src/main/java/br/ufms/facom/techms/model/dao/FuncionarioDAO.java
+++ src/main/java/br/ufms/facom/techms/model/dao/FuncionarioDAO.java
@@ -26,15 +26,15 @@ public class FuncionarioDAO {
 
     public List<Funcionario> readAll() throws SQLException {
         List<Funcionario> funcionarios = new ArrayList<>();
-        String sql = "SELECT id, nome FROM funcionario";
-        try (PreparedStatement stmt = connection.prepareStatement(sql);
+        String sql = "SELECT func.id, pess.nome FROM funcionario AS func, pessoa AS pess WHERE func.pessoa_id = pess.id;";
+        try (PreparedStatement stmt = this.connection.prepareStatement(sql);
              ResultSet rs = stmt.executeQuery()) {
             while (rs.next()) {
                 Funcionario funcionario = new Funcionario();
                 funcionario.setId(rs.getInt("id"));
                 funcionario.setNome(rs.getString("nome"));
                 funcionarios.add(funcionario);
-            }
+                }
         }
         return funcionarios;
     }
